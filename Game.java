1bd7482790e28b42a4cb83fbf9415e34b1dd7b25
TrangKhanhLy <khanh.lykhanhtrang@gmail.com>
2026-03-05 11:20:39 -0600

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,3 +1,32 @@
-public class Game {
-    
+public class Game{
+    private String name;
+    private int happinessIncr;
+    private int weightDecr;
+
+    public Game(String name, int happinessIncr, int weightDecr){
+        this.name = name;
+        this.happinessIncr = happinessIncr;
+        this.weightDecr = weightDecr;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getHappinessIncr(){
+        return happinessIncr;
+    }
+
+    public int  getWeightLoss(){
+        return weightDecr;
+    }
+
+    public boolean isWinner(){
+        int win = (int) (Math.random() * (100-0+1) + 0);
+        if(win <= 50){
+            return true;
+        } else { 
+            return false;
+        }
+    }
 }
